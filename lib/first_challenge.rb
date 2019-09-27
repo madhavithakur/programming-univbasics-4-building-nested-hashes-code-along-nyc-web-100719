dccbc70c7694d3c31f99eb97f2d65d0309d9574a
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-27 22:11:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,8 @@
-def first_challenge
-   epic_tragedy = {}
-end
+# def first_challenge
+#   # epic_tragedy = {}
+#   :montague
+# end
+
+  def first_challenge
+    epic_tragedy = {:montague => {}, :capulet => {}}
+  end 
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,26 @@
+# def second_challenge
+#   epic_tragedy = {
+#     :montague => {
+#       #your key/value pairs here!
+#       }, 
+#     :capulet => {
+#       #your key/value pairs here!
+#       }}
+# end
+
+
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+    :montague => {:patriarch, :matriarch, :hero, :hero_friends }, 
+    :capulet => {:patriarch, :matriarch, :heroine, :heroine_friends}
+     }
 end
+
+# describe "second_challenge" do
+# 	it "fill out the empty hashes that are the values of the family name keys with the appropriate key/value pairs" do
+#     expect(second_challenge[:montague].keys).to eq([:patriarch, :matriarch, :hero, :hero_friends])
+#     expect(second_challenge[:capulet].keys).to eq([:patriarch, :matriarch, :heroine, :heroine_friends])
+#     expect(second_challenge[:montague].values).to eq([{}, {}, {}, []])
+#     expect(second_challenge[:capulet].values).to eq([{}, {}, {}, []])
+
+#   end
