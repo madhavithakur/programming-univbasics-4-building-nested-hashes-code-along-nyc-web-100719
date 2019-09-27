4e54b7d3acc9e2aa9cfb820bdd50e66a28e69634
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-27 22:26:42 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,31 +1,31 @@
-def third_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
-end
+# def third_challenge
+#   epic_tragedy = {
+#   :montague => {
+#       :patriarch => {
+#         #your key/value pairs here
+#         },
+#       :matriarch => {
+#         #your key/value pairs here
+#         },
+#       :hero => {
+#         #your key/value pairs here
+#         },
+#       :hero_friends => []
+#   }, 
+#   :capulet => {
+#       :patriarch => {
+#         #your key/value pairs here
+#         },
+#       :matriarch => {
+#         #your key/value pairs here
+#         },
+#       :heroine => {
+#         #your key/value pairs here
+#         },
+#       :heroine_friends => []
+#   }
+#   }
+# end
 =====================================
 def third_challenge
   epic_tragedy = {
@@ -42,15 +42,7 @@ def third_challenge
       :hero_friends => []
    }, 
 
-describe "third_challenge" do
-	it "fills out the Montague character role keys with the key/value pairs describing their attributes" do
-    expect(third_challenge[:montague][:patriarch].keys).to eq([:name, :age])
-    expect(third_challenge[:montague][:matriarch].keys).to eq([:name, :age])
-    expect(third_challenge[:montague][:hero].keys).to eq([:name, :age, :status])
-    expect(third_challenge[:montague][:patriarch].values).to eq(["Lord Montague", "53"])
-    expect(third_challenge[:montague][:matriarch].values).to eq(["Lady Montague", "54"])
-    expect(third_challenge[:montague][:hero].values).to eq(["Romeo", "15", "alive"])
-  end
+
   
   
   
