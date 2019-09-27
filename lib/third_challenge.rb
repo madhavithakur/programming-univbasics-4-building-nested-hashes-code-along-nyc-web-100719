5ec792754e558e9b716c4f77b332e8e80c4830e3
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-27 22:38:31 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,32 +1,4 @@
-# def third_challenge
-#   epic_tragedy = {
-#   :montague => {
-#       :patriarch => {
-#         #your key/value pairs here
-#         },
-#       :matriarch => {
-#         #your key/value pairs here
-#         },
-#       :hero => {
-#         #your key/value pairs here
-#         },
-#       :hero_friends => []
-#   }, 
-#   :capulet => {
-#       :patriarch => {
-#         #your key/value pairs here
-#         },
-#       :matriarch => {
-#         #your key/value pairs here
-#         },
-#       :heroine => {
-#         #your key/value pairs here
-#         },
-#       :heroine_friends => []
-#   }
-#   }
-# end
-#=====================================
+
 def third_challenge
   epic_tragedy = {
    :montague => {
@@ -57,14 +29,6 @@ def third_challenge
   }
 end
 
-# it "fills out the Capulet character role keys with the key/value pairs describing their attributes" do
-#     expect(third_challenge[:capulet][:patriarch].keys).to eq([:name, :age])
-#     expect(third_challenge[:capulet][:matriarch].keys).to eq([:name, :age])
-#     expect(third_challenge[:capulet][:heroine].keys).to eq([:name, :age, :status])
-#     expect(third_challenge[:capulet][:patriarch].values).to eq(["Lord Capulet", "50"])
-#     expect(third_challenge[:capulet][:matriarch].values).to eq(["Lady Capulet", "51"])
-#     expect(third_challenge[:capulet][:heroine].values).to eq(["Juliet", "15", "alive"])
-#   end
 
   
   
