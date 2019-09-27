3aeb6bba609f49eda3aad053bbc9c2ec8932c851
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-27 22:19:56 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -12,7 +12,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {"patriarch": {}, "matriarch": {}, "hero": {}, "hero_friends": [] }, 
-    :capulet => {"patriarch": {}, "matriarch": {}, "heroine": {}, "heroine_friends": []]
+    :capulet => {"patriarch": {}, "matriarch": {}, "heroine": {}, "heroine_friends": []}
      }
 end
 
