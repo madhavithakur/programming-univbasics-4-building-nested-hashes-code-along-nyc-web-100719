e8a2c35c0458fe8f2a101b8458dbaeabf9885ab1
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-27 22:43:20 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,3 +27,13 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
+
+# describe "bonus" do
+#   # remove the 'x' from the 'xit' below to activate this test!
+# 	xit "uses the []= method to change the hero and heroine's status from alive to dead" do
+#     expect(bonus[:montague][:hero][:status]).to eq("dead")
+#     expect(bonus[:capulet][:heroine][:status]).to eq("dead")
+
+#   end
+# end
