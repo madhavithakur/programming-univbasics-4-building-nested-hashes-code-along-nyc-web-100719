15fe479167a3eb72865564c84acbf8e4827ca972
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-27 22:36:28 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -44,18 +44,27 @@ def third_challenge
 
 :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        "name": "Lord Capulet" , "age": "50" 
         },
       :matriarch => {
-        #your key/value pairs here
+        "name": "Lady Capulet" , "age": "51" 
         },
       :heroine => {
-        #your key/value pairs here
+        "name": "Juliet" , "age": "15" ":status" "alive"
         },
       :heroine_friends => []
   }
   }
 end
+
+ it "fills out the Capulet character role keys with the key/value pairs describing their attributes" do
+    expect(third_challenge[:capulet][:patriarch].keys).to eq([:name, :age])
+    expect(third_challenge[:capulet][:matriarch].keys).to eq([:name, :age])
+    expect(third_challenge[:capulet][:heroine].keys).to eq([:name, :age, :status])
+    expect(third_challenge[:capulet][:patriarch].values).to eq(["Lord Capulet", "50"])
+    expect(third_challenge[:capulet][:matriarch].values).to eq(["Lady Capulet", "51"])
+    expect(third_challenge[:capulet][:heroine].values).to eq(["Juliet", "15", "alive"])
+  end
 
   
   
